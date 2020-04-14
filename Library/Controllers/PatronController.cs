4d47398bbea39fc7ceb47795abd3159783951e0a
Dasun Navoda <dasunnavoda@gmail.com>
2020-04-14 10:06:54 +0530

    Added landing page

---
+++ Library/Controllers/PatronController.cs
@@ -0,0 +1,21 @@
+﻿using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class PatronController : Controller
+    {
+        private ILibraryAsset _assets;
+        private ICheckout _checkouts;
+
+        public PatronController(ILibraryAsset assets, ICheckout checkouts)
+        {
+            _assets = assets;
+            _checkouts = checkouts;
+        }
+    }
+}
