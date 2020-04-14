86282c044be933339344535407eecf858c4904e1
Dasun Navoda <dasunnavoda@gmail.com>
2020-04-14 12:41:50 +0530

    Added Patron management logic & implemented views

--- Library/Controllers/PatronController.cs
+++ Library/Controllers/PatronController.cs
@@ -4,18 +4,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Models.PatronModels;
+using LibraryData.Models;
 
 namespace Library.Controllers
 {
     public class PatronController : Controller
     {
-        private ILibraryAsset _assets;
-        private ICheckout _checkouts;
-
-        public PatronController(ILibraryAsset assets, ICheckout checkouts)
+        private IPatron _patron;
+        public PatronController(IPatron patron)
         {
-            _assets = assets;
-            _checkouts = checkouts;
+            _patron = patron;
+        }
+        public IActionResult Index()
+        {
+            var allPatrons = _patron.GetAll();
+
+            var patronModels = allPatrons.Select(p => new PatronDetailModel { 
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                LibraryCardId = p.LibraryCard.Id,
+                OverdueFees = p.LibraryCard.Fees,
+                HomeLibraryBranch = p.HomeLibraryBranch.Name
+            }).ToList();
+
+            var model = new PatronIndexModel
+            {
+                Patrons = patronModels
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var patron = _patron.Get(id);
+
+            var model = new PatronDetailModel
+            {
+                FirstName = patron.FirstName,
+                LastName = patron.LastName,
+                Address = patron.Address,
+                HomeLibraryBranch = patron.HomeLibraryBranch.Name,
+                MemberSince = patron.LibraryCard.Created,
+                OverdueFees = patron.LibraryCard.Fees,
+                LibraryCardId = patron.LibraryCard.Id,
+                Telephone = patron.TelephoneNumber,
+                AssetsCheckedOut = _patron.GetCheckouts(id).ToList() ?? new List<Checkout>(),
+                CheckoutHistory = _patron.GetCheckoutHistory(id),
+                Holds = _patron.GetHolds(id)
+            };
+
+            return View(model);
         }
     }
 }
---
+++ LibraryData/IPatron.cs
@@ -0,0 +1,18 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData
+{
+    public interface IPatron
+    {        
+        Patron Get(int id);
+        void Add(Patron newPatron);
+
+        IEnumerable<Patron> GetAll();
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int patronId);
+        IEnumerable<Hold> GetHolds(int patronId);
+        IEnumerable<Checkout> GetCheckouts(int patronId);
+    }
+}
